b2943f187a96f28505db0b2bddc4ff6d256b753c
Marley <marleyxiong0@gmail.com>
2020-11-20 18:58:35 -0700

    refactor graph

---
+++ src/cayleyGraph.js
@@ -0,0 +1,126 @@
+let test = {
+  nodes: ["1", "(12)", "(13)", "(23)", "(123)", "(132)"],
+  meta: {
+    1: {
+      order: 1,
+    },
+    //"labels": Object.fromEntries(nodes.map(nodeId => ([nodeId, nodeId == 7 ? "You" : "???"])))
+  },
+  // edges format: [node 1, node 2, generator] -when you left multiply node1 by the generator you get node 2
+  edges: [["(12)(134)", "(12)", "(13)"]],
+};
+/**
+ *
+ * @param {number} num
+ * the symetry group S_n acts on a set K (with |K|=n), num is an element of K we want to know the destination of
+ * @param {*} perms
+ * array of arrays (cycles) in written order, each sub-array is a little cycle
+ */
+function getDestination(number, perms) {
+  let reversedPerms = perms.slice().reverse();
+  //   console.log("reverse", reversedPerms);
+  // reverse perms to apply the rightmost permutation first
+  let nxt = number;
+  for (const per of reversedPerms) {
+    // for each cycle in the permutation
+    if (per.includes(nxt)) {
+      nxt = per[(per.indexOf(nxt) + 1) % per.length];
+    }
+  }
+  return nxt;
+}
+
+/**
+ *
+ * @param {*} perms
+ * 2D array of cycles to be simplified, e.g. [(1,2,3),(1,2)]
+ * @param {*} n
+ * integer, size of the permutation group
+ * @returns disjoint cycles in 2D array
+ */
+
+function mult(perms) {
+  const product = [];
+  const done = []; // elements that have been computed
+  const n = Math.max(...perms.map((el) => Math.max(...el))); // max over all perms
+
+  for (let i = 0; i < n; i++) {
+    let num = i + 1;
+    if (!done.includes(num)) {
+      const currentCycle = [];
+      while (!currentCycle.includes(num)) {
+        currentCycle.push(num);
+        let nxt = getDestination(num, perms);
+        num = nxt;
+
+        done.push(num);
+      }
+      if (currentCycle.length >= 2) {
+        product.push([...currentCycle]);
+      }
+    }
+  }
+  if (!product.length) {
+    return [[]];
+  }
+  return product;
+}
+
+// function conjugate_element()
+
+// takes a permutation in the format [[1,2,3],[4,5],[6,8,7]]
+// and converts it to the string "(123)(45)(687)"
+function permToString(perm) {
+  let stringRep = "";
+  for (let cycle of perm) {
+    stringRep = stringRep + "(";
+    for (let i of cycle) {
+      stringRep = stringRep + i.toString();
+    }
+    stringRep = stringRep + ")";
+  }
+  return stringRep;
+}
+const groupGenerators = {
+  S3: [[[1, 2]], [[1, 2, 3]]],
+  Z3: [[[1, 2, 3]]],
+  S4: [[[1, 2]], [[1, 2, 3, 4]]],
+  S5: [[[1, 2]], [[1, 2, 3, 4, 5]]],
+  A5: [
+    [
+      [1, 2],
+      [3, 4],
+    ],
+    [[1, 3, 5]],
+  ],
+};
+
+function DFS(currentNode, nGraph, generators) {
+  // add the current node and the edges to nGraph
+  let currentNodeName = permToString(currentNode);
+  nGraph.nodes.push(currentNodeName); // mark node as visited
+  for (const gen of generators) {
+    const child = mult([...gen, ...currentNode]);
+    // console.log("multiplier", [...gen, ...currentNode]);
+    // console.log("child", child);
+    const childName = permToString(child);
+    const genName = permToString(gen);
+    nGraph.edges.push([currentNodeName, childName, genName]);
+    if (!nGraph.nodes.includes(childName)) {
+      // call DFS  on child recursively
+      DFS(child, nGraph, generators);
+    }
+  }
+}
+
+export const getCayleyGraph = (groupName) => {
+  const generators = groupGenerators[groupName];
+  const currentNode = [[]];
+  let newGraph = {
+    nodes: [],
+    edges: [],
+  };
+  DFS(currentNode, newGraph, generators);
+  console.log("\nnewGraph", newGraph);
+  return newGraph;
+};
